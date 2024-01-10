9abcf80cf9bfa68ca4944c2f208d9b01d56c98f5
sseang <142060734+sseang@users.noreply.github.com>
2024-01-09 20:47:30 -0600

    Created useState function

--- src/components/components/Card/Card.jsx
+++ src/components/components/Card/Card.jsx
@@ -1,7 +1,22 @@
+//to use state you need hooks
+import {useState} from 'react';
+
 function Card() {
+  const [myName, setMyName] = useState('Sarith');
+
+  function clickChangeName() {
+    //set name to new name
+    console.log('Test Name');
+    setMyName('SARITH');
+
+  }
+
+
     return (
       <div className="card">
         <button>count is 0</button>
+        <p>{myName}</p>
+        <button onClick={clickChangeName}>Change Name</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
