664b8b482ce8c777409c31cb4f92c63703240435
sseang <142060734+sseang@users.noreply.github.com>
2024-01-09 19:52:56 -0600

    Created Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer/Footer';
-import Card  from './components/components/Card/Card';
+import Card  from '';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -20,13 +20,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-
-        <button>count is 0</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+    <Card/>
     <Footer/>
     </>
   )
